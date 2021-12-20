5c4d7ef6c9135913aca91acba856bff324452213
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-12-20 22:02:19 +0300

    Add shuffleArray function

---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,7 @@
+//https://flaviocopes.com/how-to-shuffle-array-javascript/
+
+const shuffleArray = (array) => {
+  return [...array].sort(() => Math.random() - 0.5);
+};
+
+export default shuffleArray;
