30b3e152b8b449edcc3973ba35d61c72580b343f
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-12-22 00:48:38 +0300

    Add useEffect to check pairs && update handleCardClick function

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+import Stats from "../Stats/Stats";
 import Card from "../Card/Card";
 
 import shuffleArray from "../../utils/shuffleArray";
 import importAllCardImages from "../../utils/importAllCardImages";
-import Stats from "../Stats/Stats";
-import { useEffect } from "react/cjs/react.development";
 
 const BoardStyled = styled.div`
   width: 100%;
@@ -20,6 +19,7 @@ const CardContainer = styled.div``;
 function Board() {
   const [clickCount, setClickCount] = useState(0);
   const [shuffledArray, setShuffledArray] = useState([]);
+  const [cardPair, setCardPair] = useState([]);
 
   //make an array of images
   const allCards = importAllCardImages(
@@ -30,11 +30,6 @@ function Board() {
     )
   );
 
-  //increment the click count
-  const handleCardClick = () => {
-    setClickCount(clickCount + 1);
-  };
-
   //duplicate the array for matching and shuffle it on initial render
   useEffect(() => {
     setShuffledArray([
@@ -42,6 +37,25 @@ function Board() {
       ...shuffleArray(Object.values(allCards)),
     ]);
   }, []);
+
+  //check matching status of card pairs
+  useEffect(() => {
+    if (cardPair[0] === cardPair[1] && cardPair.length) {
+      console.log("it's a match!");
+    } else {
+      console.log("will flip");
+    }
+  }, [cardPair]);
+
+  //increment the click count && update card pairs
+  const handleCardClick = (event) => {
+    setClickCount(clickCount + 1);
+    if (cardPair.length < 2) {
+      setCardPair([...cardPair, event.target.dataset.id]);
+    } else {
+      setCardPair([]);
+    }
+  };
 
   return (
     <>
