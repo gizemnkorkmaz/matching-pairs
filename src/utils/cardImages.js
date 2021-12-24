be640c52c544961fe40d8e0fad83d30555b1005e
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-12-25 01:29:36 +0300

    Add cardImages.js

---
+++ src/utils/cardImages.js
@@ -0,0 +1,41 @@
+import francrepas from "../assets/images/cards/francrepas.jpeg";
+import harbinger from "../assets/images/cards/harbinger.jpeg";
+import holofernes from "../assets/images/cards/holofernes.jpeg";
+import mouillevent from "../assets/images/cards/mouillevent.jpeg";
+import painensac from "../assets/images/cards/painensac.jpeg";
+import papeligosse from "../assets/images/cards/papeligosse.jpeg";
+
+const cardImages = [
+  {
+    name: "francrepas",
+    src: francrepas,
+    id: 1,
+  },
+  {
+    name: "harbinger",
+    src: harbinger,
+    id: 2,
+  },
+  {
+    name: "holofernes",
+    src: holofernes,
+    id: 3,
+  },
+  {
+    name: "mouillevent",
+    src: mouillevent,
+    id: 4,
+  },
+  {
+    name: "painensac",
+    src: painensac,
+    id: 5,
+  },
+  {
+    name: "papeligosse",
+    src: papeligosse,
+    id: 6,
+  },
+];
+
+export default cardImages;
