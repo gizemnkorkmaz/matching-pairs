466acd31172523784bf6c9baa03276a6ede2480b
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-12-29 02:17:43 +0300

    Add isShowLevels state & Add Levels component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import GlobalStyle from "./components/globalStyled";
 import Header from "./components/Header/Header";
 import Board from "./components/Board/Board";
 
 import data from "./data.json";
+import Levels from "./components/Levels/Levels";
 
 const duplicatedCards = [...data.cards, ...data.cards].map((card) => ({
   ...card,
@@ -10,11 +13,19 @@ const duplicatedCards = [...data.cards, ...data.cards].map((card) => ({
 }));
 
 function App() {
+  const [isShowLevels, setIsShowLevels] = useState(false);
+
   return (
     <div className="App">
       <GlobalStyle />
-      <Header />
-      <Board duplicatedCards={duplicatedCards} />
+      {isShowLevels ? (
+        <Levels />
+      ) : (
+        <>
+          <Header />
+          <Board duplicatedCards={duplicatedCards} />
+        </>
+      )}
     </div>
   );
 }
