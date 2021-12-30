7d985b3263d4ade3737bc64285de3d8180f5d6f0
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-12-30 16:50:39 +0300

    Remove Header and Board components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import { useState } from "react";
-
-import Header from "./components/Header/Header";
-import Board from "./components/Board/Board";
 import Levels from "./components/Levels/Levels";
 import GlobalStyle from "./components/globalStyled";
 
-import data from "./data.json";
-
-const duplicatedCards = [...data.cards, ...data.cards].map((card) => ({
-  ...card,
-  id: Math.random(),
-}));
-
 function App() {
-  const [isShowLevels, setIsShowLevels] = useState(false);
-
   return (
     <div className="App">
       <GlobalStyle />
-      {isShowLevels ? (
-        <Levels />
-      ) : (
-        <>
-          <Header />
-          <Board duplicatedCards={duplicatedCards} />
-        </>
-      )}
+      <Levels />
     </div>
   );
 }
