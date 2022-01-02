9ce08fc22fdbbe53de9c333cb7087389b8ef4f6d
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-02 23:20:29 +0300

    Add padding & max-width

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-const StatsStyled = styled.p`
+const StatsStyled = styled.div`
+  max-width: 1400px;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   text-align: center;
   color: #eee;
   opacity: 0.6;
   margin: 0;
-  font-size: 20px;
+  padding: 0 2rem;
+  font-size: 18px;
 `;
 
-function Stats({ movesCount }) {
-  return <StatsStyled>Moves: {movesCount}</StatsStyled>;
+function Stats({ movesCount, children }) {
+  return (
+    <StatsStyled>
+      <p>Moves: {movesCount}</p>
+      <span>{children}</span>
+    </StatsStyled>
+  );
 }
 
 export default Stats;
