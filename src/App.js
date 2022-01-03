fdb54c97418539045540ee7451d4ad5de0d53336
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-03 22:12:13 +0300

    Add setGameLevel state

--- src/App.js
+++ src/App.js
@@ -11,15 +11,19 @@ import data from "./data.json";
 function App() {
   const [isStartGame, setIsStartGame] = useState(true);
   const [duplicatedCards, setDuplicatedCards] = useState(null);
+  const [gameLevel, setGameLevel] = useState(null);
 
   const selectLevel = (level) => {
     setIsStartGame(false);
     if (level === "easy") {
       setDuplicatedCards(duplicateCards(data, 8));
+      setGameLevel("easy");
     } else if (level === "medium") {
       setDuplicatedCards(duplicateCards(data, 12));
+      setGameLevel("medium");
     } else if (level === "hard") {
       setDuplicatedCards(duplicateCards(data, 16));
+      setGameLevel("hard");
     }
   };
   return (
@@ -32,6 +36,7 @@ function App() {
         <Board
           duplicatedCards={duplicatedCards}
           setIsStartGame={setIsStartGame}
+          gameLevel={gameLevel}
         />
       )}
     </div>
