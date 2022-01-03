0831c26c1a1f26fe584035813892151c16fd20ec
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-03 19:00:56 +0300

    Add isStartGame&duplicateCards states & selectLevel method & Header, Board, Levels components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import GlobalStyle from "./components/globalStyled";
+import { useState } from "react";
+
+import Header from "./components/Header/Header";
+import Board from "./components/Board/Board";
 import Levels from "./components/Levels/Levels";
+import GlobalStyle from "./components/globalStyled";
+
+import duplicateCards from "./utils/duplicateCards";
+import data from "./data.json";
 
 function App() {
+  const [isStartGame, setIsStartGame] = useState(true);
+  const [duplicatedCards, setDuplicatedCards] = useState(null);
+
+  const selectLevel = (level) => {
+    setIsStartGame(false);
+    if (level === "easy") {
+      setDuplicatedCards(duplicateCards(data, 8));
+    } else if (level === "medium") {
+      setDuplicatedCards(duplicateCards(data, 12));
+    } else {
+      setDuplicatedCards(duplicateCards(data, 16));
+    }
+  };
   return (
     <div className="App">
       <GlobalStyle />
-      <Levels />
+      <Header />
+      {isStartGame ? (
+        <Levels selectLevel={selectLevel} />
+      ) : (
+        <Board
+          duplicatedCards={duplicatedCards}
+          setIsStartGame={setIsStartGame}
+        />
+      )}
     </div>
   );
 }
