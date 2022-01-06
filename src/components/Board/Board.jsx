6c3d06a883f9fe369b45804109b03a3110d5762f
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-06 17:36:37 +0300

    Add calculateScore function

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -12,7 +12,6 @@ const BoardStyled = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 0.5rem;
-  padding: 1rem;
   justify-content: center;
   max-width: 1400px;
 `;
@@ -22,6 +21,8 @@ function Board({ duplicatedCards, setIsStartGame, gameLevel }) {
   const [cardPair, setCardPair] = useState([]);
   const [flippedCardList, setFlippedCardList] = useState([]);
   const [movesCount, setMovesCount] = useState(0);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   const handleCardClick = (id) => {
     if (cardPair.includes(id)) return;
@@ -35,6 +36,7 @@ function Board({ duplicatedCards, setIsStartGame, gameLevel }) {
 
       if (firstCardName === secondCardName) {
         setFlippedCardList([...flippedCardList, ...currentCardPair]);
+        calculateScore();
       }
 
       setCardPair(currentCardPair);
@@ -52,15 +54,28 @@ function Board({ duplicatedCards, setIsStartGame, gameLevel }) {
     setCardPair([]);
     setFlippedCardList([]);
     setMovesCount(0);
+    setScore(0);
   };
 
   const changeLevel = () => {
     setIsStartGame(false);
   };
 
+  const calculateScore = () => {
+    const cardCount = cards.length / 2;
+    const flippedCardCount = flippedCardList.length + 2;
+    const currentScore = Math.floor((cardCount / movesCount) * 1000);
+
+    setScore(currentScore);
+
+    if (flippedCardCount === cards.length && currentScore > highScore) {
+      setHighScore(currentScore);
+    }
+  };
+
   return (
     <>
-      <Stats movesCount={movesCount}>
+      <Stats score={score} highScore={highScore}>
         <Button handleClick={resetGame}>Reset Game</Button>
         <Button handleClick={changeLevel}>Change Difficulty</Button>
       </Stats>
